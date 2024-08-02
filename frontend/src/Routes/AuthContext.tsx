0709288b1c8ecdfd54e3b71d8f0ed118933e1aa6
Bechir Ben Jazia <bechir.benjazia8@gmail.com>
2024-08-03 00:28:51 +0100

    Auth

--- frontend/src/Routes/AuthContext.tsx
+++ frontend/src/Routes/AuthContext.tsx
@@ -14,7 +14,7 @@ interface AuthContextType {
   logout: () => void;
 }
 
-const AuthContext = createContext<AuthContextType | undefined>(undefined);
+export const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider: React.FC<{ children: ReactNode }> = ({
   children,
